cc3af860a86e7c1cef1ff0bb9cc0582128588570
ShoMasegi <m2.b0c27@gmail.com>
2019-11-10 18:45:18 +0900

    🚚 :Setup Directory

--- app/src/main/java/com/sho/masegi/tutorial/ui/MainActivity.kt
+++ app/src/main/java/com/sho/masegi/tutorial/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.sho.masegi.tutorial
+package com.sho.masegi.tutorial.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.sho.masegi.model.User
+import com.sho.masegi.tutorial.R
 import com.sho.masegi.tutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
