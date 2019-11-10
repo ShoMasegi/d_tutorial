6ebad3d49c60991f36d2dd69205983822cf0e18a
ShoMasegi <m2.b0c27@gmail.com>
2019-11-11 03:15:19 +0900

    🎨 :Implement NetworkModule

--- network/src/main/java/com/sho/masegi/network/di/NetworkModule.kt
+++ network/src/main/java/com/sho/masegi/network/di/NetworkModule.kt
@@ -1,6 +1,24 @@
 package com.sho.masegi.network.di
 
+import com.sho.masegi.network.GitHubApi
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
-class NetworkModule {}
+class NetworkModule {
+    @Singleton @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    @Singleton @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://api.github.com")
+            .build()
+
+    @Singleton @Provides
+    fun provideGitHubApi(retrofit: Retrofit): GitHubApi = retrofit.create(GitHubApi::class.java)
+}
