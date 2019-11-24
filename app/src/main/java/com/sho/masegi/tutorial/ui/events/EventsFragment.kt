023919d2e376207d515ded2683160e5071cc17b5
ShoMasegi <m2.b0c27@gmail.com>
2019-11-24 17:24:03 +0900

    ✨ :Inject EventsViewModel to EventsFragment

--- app/src/main/java/com/sho/masegi/tutorial/ui/events/EventsFragment.kt
+++ app/src/main/java/com/sho/masegi/tutorial/ui/events/EventsFragment.kt
@@ -1,17 +1,24 @@
 package com.sho.masegi.tutorial.ui.events
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.sho.masegi.tutorial.MainApplication
 
 import com.sho.masegi.tutorial.R
+import javax.inject.Inject
 
 class EventsFragment : Fragment() {
 
-    private lateinit var viewModel: EventsViewModel
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val viewModel: EventsViewModel by viewModels { viewModelFactory }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,9 +27,13 @@ class EventsFragment : Fragment() {
         return inflater.inflate(R.layout.events_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(EventsViewModel::class.java)
-    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (activity!!.application as MainApplication).appComponent()
+            .eventsComponent()
+            .create()
+            .inject(this)
 
+        Log.d("Debug","ViewModel: $viewModel")
+    }
 }
