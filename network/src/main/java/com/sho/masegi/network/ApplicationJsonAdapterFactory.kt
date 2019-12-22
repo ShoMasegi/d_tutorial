155745315a444c99c8d1b5e31c08ef4ea51b40f6
ShoMasegi <m2.b0c27@gmail.com>
2019-12-22 19:33:34 +0900

    🎨 :Create Kotshi JsonAdapterFactory

---
+++ network/src/main/java/com/sho/masegi/network/ApplicationJsonAdapterFactory.kt
@@ -0,0 +1,11 @@
+package com.sho.masegi.network
+
+import com.squareup.moshi.JsonAdapter
+import se.ansman.kotshi.KotshiJsonAdapterFactory
+
+@KotshiJsonAdapterFactory
+abstract class ApplicationJsonAdapterFactory : JsonAdapter.Factory {
+    companion object {
+        val instance: ApplicationJsonAdapterFactory = KotshiApplicationJsonAdapterFactory
+    }
+}
