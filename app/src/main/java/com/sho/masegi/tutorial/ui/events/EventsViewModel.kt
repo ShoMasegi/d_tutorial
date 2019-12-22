036112db3589027df2a7f3e7a3dc6a3bce9dfb86
ShoMasegi <m2.b0c27@gmail.com>
2019-12-22 19:50:41 +0900

    🎨 :Add events property to EventViewModel

--- app/src/main/java/com/sho/masegi/tutorial/ui/events/EventsViewModel.kt
+++ app/src/main/java/com/sho/masegi/tutorial/ui/events/EventsViewModel.kt
@@ -1,9 +1,17 @@
 package com.sho.masegi.tutorial.ui.events
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.LiveData
+import com.sho.masegi.model.Event
 import com.sho.masegi.network.GitHubApi
 import javax.inject.Inject
 
 class EventsViewModel @Inject constructor(
-    val githubApi: GitHubApi
-) : ViewModel()
+    private val githubApi: GitHubApi
+) : ViewModel() {
+    val events: LiveData<List<Event>> = liveData {
+        val value = githubApi.events(1)
+        emit(value)
+    }
+}
