865db42b33b40b9a557458c43571aaa23b77db41
ShoMasegi <m2.b0c27@gmail.com>
2019-12-22 19:37:21 +0900

    🎨 :Add Kotshi converter to network module

--- network/src/main/java/com/sho/masegi/network/di/NetworkModule.kt
+++ network/src/main/java/com/sho/masegi/network/di/NetworkModule.kt
@@ -1,10 +1,14 @@
 package com.sho.masegi.network.di
 
+import com.sho.masegi.network.ApplicationJsonAdapterFactory
 import com.sho.masegi.network.GitHubApi
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import retrofit2.Converter
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -13,10 +17,22 @@ class NetworkModule {
     fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
 
     @Singleton @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+    fun provideConverterFactory(): Converter.Factory {
+        val moshi = Moshi.Builder()
+            .add(ApplicationJsonAdapterFactory.instance)
+            .build()
+        return MoshiConverterFactory.create(moshi)
+    }
+
+    @Singleton @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: Converter.Factory
+    ): Retrofit =
         Retrofit.Builder()
             .client(okHttpClient)
             .baseUrl("https://api.github.com")
+            .addConverterFactory(converterFactory)
             .build()
 
     @Singleton @Provides
