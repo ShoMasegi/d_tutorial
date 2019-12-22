f082a8697e86b72fb4ea6b40430a687d95b7f8a0
ShoMasegi <m2.b0c27@gmail.com>
2019-12-22 19:52:08 +0900

    💄 :Use databinding in EvnetsFragment

--- app/src/main/java/com/sho/masegi/tutorial/ui/events/EventsFragment.kt
+++ app/src/main/java/com/sho/masegi/tutorial/ui/events/EventsFragment.kt
@@ -7,24 +7,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.sho.masegi.tutorial.MainApplication
 
 import com.sho.masegi.tutorial.R
+import com.sho.masegi.tutorial.databinding.EventsFragmentBinding
 import javax.inject.Inject
 
 class EventsFragment : Fragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+    private lateinit var binding: EventsFragmentBinding
     private val viewModel: EventsViewModel by viewModels { viewModelFactory }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.events_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.events_fragment, container, false)
+        return binding.root
     }
 
     override fun onAttach(context: Context) {
