caf27704d866dcb149858e5b8d8e40bd7f1d247a
ShoMasegi <m2.b0c27@gmail.com>
2019-12-22 19:48:05 +0900

    🎨 :Implements 'GET /events' API

--- network/src/main/java/com/sho/masegi/network/GitHubApi.kt
+++ network/src/main/java/com/sho/masegi/network/GitHubApi.kt
@@ -1,9 +1,10 @@
 package com.sho.masegi.network
 
+import com.sho.masegi.model.Event
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GitHubApi {
     @GET("/events")
-    suspend fun events(@Query("page") page: Int): Unit
+    suspend fun events(@Query("page") page: Int): List<Event>
 }
