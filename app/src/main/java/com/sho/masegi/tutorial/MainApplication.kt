0438325f817420ef070d5e0fc5b86af851b9f1e0
ShoMasegi <m2.b0c27@gmail.com>
2019-12-22 22:08:46 +0900

    🎨 :Set up AndroidThreeTen

--- app/src/main/java/com/sho/masegi/tutorial/MainApplication.kt
+++ app/src/main/java/com/sho/masegi/tutorial/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.sho.masegi.tutorial
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.sho.masegi.tutorial.di.AppComponent
 import com.sho.masegi.tutorial.di.DaggerAppComponent
 
@@ -10,4 +11,9 @@ class MainApplication: Application() {
             .builder()
             .context(applicationContext)
             .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
 }
